7f7a01a96e3b8fdb0fe1189f9de9736bd789d7e6
Valde1998 <124771370+Valde1998@users.noreply.github.com>
2024-09-09 16:04:56 +0200

    Coin

--- DungeonGame/Assets/Scripts/CoinScript.cs
+++ DungeonGame/Assets/Scripts/CoinScript.cs
@@ -8,14 +8,15 @@ public class Coin : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
+        Debug.Log("Hit");
         // Tjekker om den genstand, der kolliderer med mønten, er spilleren
         if (other.gameObject.CompareTag("Player"))
         {
             // Tilføjer point til spillerens score
             scoreManager.AddPoints(coinValue);
-
+            Debug.Log("Hit player");
             // Ødelæg mønten efter at spilleren har samlet den op
-            Destroy(this);
+            Destroy(gameObject);
         }
     }
 }
