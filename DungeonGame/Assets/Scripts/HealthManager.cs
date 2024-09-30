092d1c22f5dfb0de5b2e3419c4300265376ec0db
Valde1998 <124771370+Valde1998@users.noreply.github.com>
2024-09-30 16:59:37 +0200

    .

--- DungeonGame/Assets/Scripts/HealthManager.cs
+++ DungeonGame/Assets/Scripts/HealthManager.cs
@@ -1,47 +1,56 @@
+using System;
 using System.Collections;
 using UnityEngine;
 
-public class HealthManager : MonoBehaviour{
+public class HealthManager : MonoBehaviour
+{
     private int baseHealth = 3;
     private int currentPlayerHealth;
     private bool isPlayerDead;
 
     public Health healthUI; // Reference til Health scriptet
 
-    private void Start(){
+    private void Start()
+    {
         InitPlayerHealth();
     }
 
-    private void Update(){
-        if (currentPlayerHealth <= 0 && !isPlayerDead) {
+    private void Update()
+    {
+        if (currentPlayerHealth <= 0 && !isPlayerDead)
+        {
             StartCoroutine(KillPlayer());
         }
     }
 
-    private void InitPlayerHealth(){
-        currentPlayerHealth = baseHealth;
+    private void InitPlayerHealth()
+    {
+        currentPlayerHealth = 3;
         isPlayerDead = false;
-        UpdateHealthUI(); // Opdaterer UI med spillerens helbred
+        Debug.Log("Health: " + currentPlayerHealth);
     }
 
-    public void TakeDamage(){
-        if (!isPlayerDead) {
+    public void TakeDamage()
+    {
+        if (!isPlayerDead)
+        {
             currentPlayerHealth--;
             Debug.Log("Health: " + currentPlayerHealth);
-            UpdateHealthUI(); // Opdaterer UI når spilleren tager skade
         }
     }
 
-    private IEnumerator KillPlayer(){
+    private IEnumerator KillPlayer()
+    {
         isPlayerDead = true;
         Debug.Log("Player is dead, Restarting to 3 lives - Change later so game restarts or whatever you want to do");
         yield return new WaitForSeconds(1); // Add a delay of 1 second
         InitPlayerHealth();
     }
 
-    // Opdaterer UI
-    private void UpdateHealthUI(){
-        if (healthUI != null) {
+    private void UpdateHealthUI()
+    {
+        if (healthUI != null)
+        {
             healthUI.health = currentPlayerHealth; // Opdaterer Health scriptet
         }
     }
