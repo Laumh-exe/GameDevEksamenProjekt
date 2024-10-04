9745ee445e337781aae3feda7b1ac8165411e217
Laumh-exe <lauritzhauge@gmail.com>
2024-10-04 12:21:08 +0200

    First version -- willprobably redo
    Doesnt work well, will redo next commit maybe

--- DungeonGame/Assets/Scripts/PlayerMovement.cs
+++ DungeonGame/Assets/Scripts/PlayerMovement.cs
@@ -6,9 +6,14 @@ using UnityEngine.InputSystem.LowLevel;
 using UnityEngine.Serialization;
 
 public class PlayerMovement : MonoBehaviour{
+    [Header("CameraMovement")] 
+    [SerializeField] private GameObject followTarget;
+    [SerializeField] private float rotationPower = 3f;
+    
     [Header("Movement")] [SerializeField] private float moveSpeed;
     [SerializeField] private float groundDrag;
     [SerializeField] private float jumpHeight;
+    [SerializeField] private float rotationLerp;
 
     [Header("Ground Check")] [SerializeField]
     private float playerHeight;
@@ -24,10 +29,14 @@ public class PlayerMovement : MonoBehaviour{
     private Vector3 rotateDirection;
     private Rigidbody rb;
     private PlayerInputs playerInputs;
+    private Quaternion nextRotation;
+    private Vector3 nextPosition;
+    private Vector3 angles;
 
     private void Awake(){
         playerInputs = new PlayerInputs();
         playerInputs.Player.Enable();
+        playerInputs.CameraMovement.Enable();
     }
 
     private void Start(){
@@ -36,6 +45,10 @@ public class PlayerMovement : MonoBehaviour{
     }
 
     private void Update(){
+        Vector2 cameraMovement = playerInputs.CameraMovement.Move.ReadValue<Vector2>();
+        VerticalCameraRotation(cameraMovement.x);
+        HorizontalCameraRotation(cameraMovement.y);
+        
         grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * .5f + .2f, whatIsGround);
         SpeedControl();
 
@@ -66,10 +79,10 @@ public class PlayerMovement : MonoBehaviour{
         horizontalInput = GetMovementVectorNormalized().x;
         verticalInput = GetMovementVectorNormalized().y;
         moveDirection = transform.forward * verticalInput;
-        rotateDirection = transform.right * horizontalInput;
+        //rotateDirection = transform.right * horizontalInput;
 
-        const float rotateSpeed = 3f;
-        transform.forward = Vector3.Slerp(transform.forward, rotateDirection.normalized, Time.deltaTime * rotateSpeed);
+        //const float rotateSpeed = 3f;
+        //transform.forward = Vector3.Slerp(transform.forward, rotateDirection.normalized, Time.deltaTime * rotateSpeed);
 
         rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
     }
@@ -81,6 +94,64 @@ public class PlayerMovement : MonoBehaviour{
             Vector3 limitedVel = flatVel.normalized * moveSpeed;
             rb.linearVelocity = new Vector3(limitedVel.x, rb.linearVelocity.y, limitedVel.z);
         }
+    }
+    
+    private void VerticalCameraRotation(float y){
+        if (!Input.GetMouseButton(0)) {
+            followTarget.transform.rotation *= Quaternion.AngleAxis(y * rotationPower, Vector3.up);
+            ClampVerticalCameraRotation();
+        }
+        
+    }
+
+    private void HorizontalCameraRotation(float x){
+        if (!Input.GetMouseButton(0)) {
+            followTarget.transform.rotation *= Quaternion.AngleAxis(x * rotationPower, Vector3.right);
+        }
+    }
+    
+    private void ClampVerticalCameraRotation(){
+        angles = followTarget.transform.localEulerAngles;
+        angles.z = 0;
+
+        var angle = followTarget.transform.localEulerAngles.x;
+
+        //Clamp the Up/Down rotation
+        if (angle > 180 && angle < 340) {
+            angles.x = 340;
+        }
+        else if (angle < 180 && angle > 40) {
+            angles.x = 40;
+        }
+
+        followTarget.transform.localEulerAngles = angles;
+    }
+
+    private void RotatePlayer(){
+        nextRotation = Quaternion.Lerp(followTarget.transform.rotation, nextRotation, Time.deltaTime * rotationLerp);
+
+        if (horizontalInput == 0 && verticalInput == 0) 
+        {   
+            nextPosition = transform.position;
+
+            if (Input.GetMouseButton(0)) // DOESNT WORK
+            {
+                Debug.Log("Mouse button is pressed");
+                //Set the player rotation based on the look transform
+                transform.rotation = Quaternion.Euler(0, followTarget.transform.rotation.eulerAngles.y, 0);
+                //reset the y rotation of the look transform
+                followTarget.transform.localEulerAngles = new Vector3(angles.x, 0, 0);
+            }
+
+            return; 
+        }
+        
+
+        //Set the player rotation based on the look transform
+        var rotation = Quaternion.Euler(0, followTarget.transform.rotation.eulerAngles.y, 0);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotationLerp);
+        //reset the y rotation of the look transform
+        followTarget.transform.localEulerAngles = new Vector3(angles.x, 0, 0);
     }
 
     private Vector2 GetMovementVectorNormalized(){
@@ -102,5 +173,9 @@ public class PlayerMovement : MonoBehaviour{
 
     private void OnDestroy(){
         playerInputs.Disable();
+    }
+
+    private void LateUpdate(){
+        RotatePlayer();
     }
 }
